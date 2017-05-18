fe9009663a4259aedee6f482c7d4ec73237aca85
stw-developer <wicksdev@gmail.com>
2017-05-18 17:32:58 +1000

    Updated top level description in comments section

--- hh_analysis.r
+++ hh_analysis.r
@@ -1,8 +1,15 @@
+
 #################################################################
 # hh_analysis.r - poker hand history analyser
 # hand history analyzer for PokerStars
 # analyzes handhistory text file from pokerstars and
-# saves to csv.
+# saves to csv. Also outputs Running profit balance, profit/hand
+# running profit graphs.
+#
+# Calculates the following:
+# - Hand statistics (hands (dealt, played, won) in absolute and % terms)
+# - Performance (bb/100, VFIP, PFR)
+# - Profitability (net profit, average profit, 100-hand average) 
 #
 # Author: sherhan wicky
 # Date  : 30/04/2017
